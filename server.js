8474f4c18b80bc37619b88e11f1a84ab7eb96b2c
Asmaul Hasnat <asmaulhasnat1311@gmail.com>
2021-04-16 00:59:43 +0600

    add controller and database connection

--- server.js
+++ server.js
@@ -2,6 +2,26 @@ const express = require('express');
 const  app = express();
 const  ejs = require('ejs');
 const  path = require('path');
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+
+//Database connection
+const url= 'mongodb://127.0.0.1:27017/pizza';
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
+});
+
+
+
 const expressLayout = require('express-ejs-layouts');
 // use assets
 app.use(express.static('public'));
@@ -13,20 +33,9 @@ app.set('view engine','ejs');
 
 
 const PORT = process.env.PORT || 3000;
+ // add web route
+require('./routes/web')(app);
 
-app.get('/',(req,res)=>{
-    res.render('home');
-})
-
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart');
-})
-app.get('/login',(req,res)=>{
-    res.render('auth/login');
-})
-app.get('/register',(req,res)=>{
-    res.render('auth/register');
-})
 
 
 app.listen(PORT,()=>{
